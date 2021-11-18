44a2864e5400f13615753e4995143c7f50616f95
Brian Mayers <brma8888@colorado.edu>
2021-11-18 13:52:14 -0700

    host-ip cahcing done, NEED: Sync, page caching

--- webproxy_test.c
+++ webproxy_test.c
@@ -13,6 +13,7 @@ Web Proxy
 #include <pthread.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <openssl/md5.h>
 
 #define MAX_URL_SIZE 500
 #define MAX_REQ_SIZE 8192
@@ -26,7 +27,9 @@ typedef enum {
 
 //pass mutex and connection fd for threads
 typedef struct {
-
+    int conn_fd;
+    pthread_mutex_t ip_mutex;
+    pthread_mutex_t cache_mutex;
 } thread_args;
 
 //hold req info
@@ -42,8 +45,9 @@ typedef struct {
 
 //holds get info
 typedef struct {
-    
+
 } HTTP_RESPONSE;
+
 
 /*
 Function to bind the server to a port
@@ -113,23 +117,71 @@ void parse_req(char * http_req, HTTP_REQUEST *new_request, int connection_fd) {
 
 /*
 Check blacklisted names in blocked.txt
+NOTE: I'm assuming that this file does not change while the program is running hence no thread synchronization
 */
-void check_blacklisted(struct hostent* host, char* hostname) {
+int check_blacklisted(char* ip_addr, char* hostname) {
     FILE* fp;
     char* line = NULL;
     ssize_t ret;
     size_t len;
-    int i = 0;
+    int i = 0, blocked = 0;
 
-    char* IP_addr = inet_ntoa(*((struct in_addr*) host->h_addr_list[0])); // get IP addr as a string
     fp = fopen("blocked.txt", "r");
-    if (fp == NULL) return; //
+    if (fp == NULL) return 1; 
 
+
+    //check if it matches the IP or the hostname in the file
     while( (ret = getline(&line, &len, fp)) > 0) {
-        
+        if (strncmp(hostname, line, strlen(hostname)) == 0) { blocked=1; break;}
+        if (strncmp(ip_addr,line, strlen(ip_addr)) == 0) {blocked = 1; break;}
     }
-    
+    if (line != NULL) free(line);
+    if ( blocked == 1) return 1;
+
     fclose(fp);
+    return 0;
+}
+ 
+/*
+Check if hostname is cached
+*/
+int check_ip_cache(char* hostname, char* ip) {
+    char* line = NULL;
+    ssize_t ret;
+    size_t len;
+    int found = 0;
+
+    FILE* fp = fopen("ip_cache.txt", "a+");
+    if (fp == NULL) return 1;
+    fseek(fp, 0, 0); //move to start of file
+
+    //search for ip address in file
+    while (ret = getline(&line, &len, fp) > 0) {
+        sscanf(line, "%*s %s", ip);
+        strtok(line, " ");
+        if (strncmp(hostname, line, strlen(hostname)) == 0) {found =1; break;}
+    }
+    if (line != NULL) free(line);
+    if (found == 1) return 0;
+
+    fclose(fp);
+    return 1;
+}
+
+/*
+Store cached ip addresses in a file
+*/
+void add_ip_cache(char* hostname, char* ip_addr) {
+    FILE* fp = fopen("ip_cache.txt", "a");
+    fprintf(fp, "%s %s \n", hostname, ip_addr); //add line to the file
+    fclose(fp);
+}
+
+/*
+Removes the hostname-ip mapping after sigint (Not Implemented yet)
+*/
+void ip_free() {
+
 }
 
 /*
@@ -139,14 +191,14 @@ void handle_error(int connection_fd, ERROR e) {
     char error_msg[500];
     memset(error_msg, 0, 500);
     switch (e) {
-        case 1:
-            sprintf(error_msg, "HTTP/1.1 400 Bad Request");
+        case 0:
+            sprintf(error_msg, "HTTP/1.1 400 Bad Request\n");
             break;
-        case 2:
+        case 1:
             sprintf(error_msg, "HTTP/1.1 404 Not Found\n");
             break;
-        case 3:
-            sprintf(error_msg, "HTTP/1.1 403 Forbidden");
+        case 2:
+            sprintf(error_msg, "HTTP/1.1 403 Forbidden\n");
             break;
     }
 
@@ -156,7 +208,7 @@ void handle_error(int connection_fd, ERROR e) {
 /*
 Sets up a new TCP connection for with the intended server
 */
-int contact_server(char *http_req, struct hostent* host, char* url, char* version, int port_num, int connection_fd, int req_size) {
+int contact_server(char* ip_addr, char* url, char* version, char* hostname, int port_num, int connection_fd, int req_size) {
     int sockfd, connfd, b_read = 0, b_write = 0;
     struct sockaddr_in servaddr, cli;
     char file_contents[PACKET_SIZE];
@@ -167,13 +219,15 @@ int contact_server(char *http_req, struct hostent* host, char* url, char* versio
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) return -1;
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    memcpy(&servaddr.sin_addr, host->h_addr_list[0], host->h_length);
+    inet_pton(AF_INET, ip_addr, &(servaddr.sin_addr));
+    //memcpy(&servaddr.sin_addr, inet_addr(ip_addr), strlen(ip_addr));
+    //memcpy(&servaddr.sin_addr, host->h_addr_list[0], host->h_length);
     servaddr.sin_port = htons(port_num);
     if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0) return -1;
 
-    //build a new HTTP REQ
+    //build a new HTTP REQ Close the connection once the page is recieved 
     bzero(request_buffer, 512);
-    sprintf(request_buffer, "GET %s %s\nhost: %s\n\n", url, version, host->h_name);
+    sprintf(request_buffer, "GET %s %s\r\nhost: %s\r\nConnection: close\r\n\r\n", url, version, hostname);
 
     //pass on http_req to server   
     write(sockfd, request_buffer, strlen(request_buffer));
@@ -181,7 +235,6 @@ int contact_server(char *http_req, struct hostent* host, char* url, char* versio
 
     while ( (n = read(sockfd, file_contents, PACKET_SIZE)) > 0) {
         write(connection_fd, file_contents, n);
-        bzero(request_buffer, PACKET_SIZE);
     }
 
     close(sockfd); //close connection after read
@@ -195,18 +248,23 @@ void get_req(int connection_fd) {
     struct hostent* host;
 
     HTTP_REQUEST *new_req = (HTTP_REQUEST*) malloc(sizeof(HTTP_REQUEST));
+    char* ip_addr = (char *)malloc(sizeof(20));
+
 
     memset(http_req, 0, MAX_REQ_SIZE);
     int n = read(connection_fd, http_req, MAX_REQ_SIZE);
     parse_req(http_req, new_req, connection_fd);
 
-    //check if a valid hostname
-    if ((host = gethostbyname(new_req->hostname)) == NULL) { handle_error(connection_fd, NOT_FOUND); return; }
-
-    check_blacklisted(host, new_req->hostname);
-
+    //check if cached
+    if (check_ip_cache(new_req->hostname, ip_addr) == 0);
+    //DNS lookup if not cached
+    else if  ((host = gethostbyname(new_req->hostname)) == NULL) { handle_error(connection_fd, NOT_FOUND); return; }
+    //convert to dotted noation and cache
+    else { ip_addr = inet_ntoa(*((struct in_addr*) host->h_addr_list[0])); add_ip_cache(new_req->hostname, ip_addr);}
+    //check if IP/hostname is blacklisted
+    if (check_blacklisted(ip_addr, new_req->hostname) == 1) { handle_error(connection_fd, FORBIDDEN); return;}
     //send new HTTP message
-    if ( contact_server(http_req, host, new_req->path, new_req->version, new_req->port, connection_fd, n) == -1) exit(0);
+    if ( contact_server(ip_addr, new_req->path, new_req->version, new_req->hostname, new_req->port, connection_fd, n) == -1) exit(0);
 
     free(new_req);
 }
@@ -217,7 +275,7 @@ thread routine
 void *handle_connection(void *thread_args) {
     int connection_fd = *((int *)thread_args);
     pthread_detach(pthread_self()); //no need to call pthread_join()
-    free(thread_args); //free space
+    free(thread_args); 
     get_req(connection_fd);
     close(connection_fd); //client can now stop waiting
     return NULL;
@@ -225,14 +283,19 @@ void *handle_connection(void *thread_args) {
 
 
 int main(int argc, char** argv) {
-    int serverfd, *connect_fd, port, clientlen=sizeof(struct sockaddr_in);
+    int serverfd, *connect_fd, port, clientlen=sizeof(struct sockaddr_in), timeout = -1;
     struct sockaddr_in clientaddr;
     char *ptr;
     pthread_t thread_id;
 
-    if (argc < 2) {
+    if (argc < 2 || argc > 3) {
         fprintf(stderr, "Usage %s <port> <timeout>\n", argv[0]);
         exit(0);
+    }
+
+    if (argc == 3) {
+        timeout = strtol(argv[2], &ptr, 10);
+        if (*ptr != '\0') { printf("Invalid Timeout\n"); exit(0); } //check for errors
     }
 
     port = strtol(argv[1], &ptr, 10);
@@ -246,5 +309,8 @@ int main(int argc, char** argv) {
         *connect_fd = accept(serverfd, (struct sockaddr *)&clientaddr, &clientlen); //start accepting requests
         pthread_create(&thread_id, NULL, handle_connection, connect_fd); //pass new file descripotr to thread routine
     }
+
+    //TODO: free with sigint handler (NOTE infinte loop)
+
     return 0;
 }
